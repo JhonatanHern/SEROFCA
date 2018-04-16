8a73b76cf037833b8c74214e54ff6060d7aa076e
JhonatanHern <jhonatanhernandez998@gmail.com>
2018-04-16 14:06:30 -0400

    remover unnecesary files and mail modified

--- mail.php
+++ mail.php
@@ -1,18 +1,28 @@
 <?php
-	$to      = 'sistemas.serofca@gmail.com';
-	$subject = 'contacto - serofca.com';
-    $name = $_GET['name'];
-    $email = $_GET['mail'];
-    $empresa = $_GET['org'];
-    $cel = $_GET['phone'];
-    $msg = $_GET['msg'];
-        
-	$message = "de: $name ($email)\n De la empresa $empresa\n$cel\n\n$msg";
-	$headers = 'From: notificaciones@serofca.com' . "\r\n" .
-	    'Reply-To: notificaciones@serofca.com' . "\r\n" .
-	    'X-Mailer: PHP/' . phpversion();
+	if (isset($_GET['mail'])) {
+		# code...
+		$to      = 'sistemas.serofca@gmail.com';
+		$subject = 'contacto - serofca.com';
+	    $name = $_GET['name'];
+	    $email = $_GET['mail'];
+	    $empresa = $_GET['org'];
+	    $cel = $_GET['phone'];
+	    $msg = $_GET['msg'];
+	        
+		$message = "de: $name ($email)\n De la empresa $empresa\n$cel\n\n$msg";
+		$headers = 'From: notificaciones@serofca.com' . "\r\n" .
+		    'Reply-To: notificaciones@serofca.com' . "\r\n" .
+		    'X-Mailer: PHP/' . phpversion();
 
-	mail($to, $subject, $message, $headers);
-?>
-<h1>Mensaje enviado</h1>
-<script>setTimeout('window.history.back()',2000)</script>
+		mail($to, $subject, $message, $headers);
+		?>
+			<h1>Mensaje enviado</h1>
+			<script>setTimeout('window.history.back()',2000)</script>
+		<?php
+	}else{
+		?>
+			<h1>Error, correo faltante</h1>
+			<script>setTimeout('window.history.back()',2000)</script>
+		<?php
+	}
+ ?>
